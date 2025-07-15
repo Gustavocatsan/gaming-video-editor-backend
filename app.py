036a6cddbbf7fa5de681fe9a7f660ea08448e319
Gustavocatsan <gustavocatsanapb@gmail.com>
2025-07-15 04:44:13 -0300

    Add wsgi file and update app to use fixed port
    Atualização para implantação na Render com Gunicorn.

--- app.py
+++ app.py
@@ -10,5 +10,4 @@ def index():
     return 'Gaming Video Editor Backend is running!'
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
